3b62b1fc86b483e6820c826477ae23f69298dd97
Phelipe Mascarenhas <ph.masc@gmail.com>
2024-10-19 14:04:34 -0300

    Restart Projetc

--- src/service/calcIdade.ts
+++
@@ -1,6 +0,0 @@
-export default function calcIdade(nascimento: Date, refencia: Date) {
-    const idade: number = (new Date(refencia.getFullYear(), nascimento.getMonth(), nascimento.getDay()) < refencia)
-        ? refencia.getFullYear() - nascimento.getFullYear() - 1
-        : refencia.getFullYear() - nascimento.getFullYear()
-    return idade
-}
